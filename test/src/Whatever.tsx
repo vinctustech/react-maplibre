e6508cd5d82deddc721bb3817cbc7561b39aaa8d
Edward A Maxedon Sr <edadma@gmail.com>
2024-08-29 14:37:57 -0400

    SC-1045 fix map not showing when reused

--- test/src/Whatever.tsx
+++ test/src/Whatever.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import { ModeProvider, ThemeProvider, Text } from '@edadma/react-tailwind'
 
 export const Whatever: FC = () => {
-  console.log('whatever')
   return (
     <ThemeProvider>
       <ModeProvider>
